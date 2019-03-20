9e030b8df07932a90ce6e68531852f3beb0a8e12
Tolulope Odueke <tolulope.od@gmail.com>
2019-03-20 23:46:45 +0100

    (feature) Create landing/home page

---
+++ UI/js/landingPage.js
@@ -0,0 +1,15 @@
+const scrollFunction = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "35px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar-right").style.fontSize = "15px";
+  } else {
+    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar-right").style.fontSize = "18px";
+  }
+};
+
+window.onscroll = () => {
+  scrollFunction();
+};
