bf251f567b26efb4bd6d45027b3c399188bc10b2
Tolulope Odueke <tolulope.od@gmail.com>
2019-04-05 12:43:08 +0100

    chore(coveralls):improve test coverage
    Refactor codebase to improve test coverage and remove irrelevant blocks of code
    
    [Finishes #165145835]

--- server/dummyControllers/AuthController.js
+++ server/dummyControllers/AuthController.js
@@ -17,51 +17,44 @@ const AuthController = {
    * @access public
    */
   signUp(req, res) {
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const { firstName, lastName, email, password, password2 } = req.body;
-      const existingUser = users.some(user => user.email === email);
-      if (!existingUser) {
-        const newUser = new User();
-        const usersLength = users.length;
-        const lastID = users[usersLength - 1].id;
-        const newID = lastID + 1;
-        newUser.id = newID;
-        newUser.firstName = firstName.trim();
-        newUser.lastName = lastName.trim();
-        newUser.email = email.trim();
-        newUser.password = password.trim();
-        newUser.type = 'staff';
-        newUser.createdAt = new Date();
+    // eslint-disable-next-line no-unused-vars
+    const { firstName, lastName, email, password, password2 } = req.body;
+    const existingUser = users.some(user => user.email === email);
+    if (!existingUser) {
+      const newUser = new User();
+      const usersLength = users.length;
+      const lastID = users[usersLength - 1].id;
+      const newID = lastID + 1;
+      newUser.id = newID;
+      newUser.firstName = firstName.trim();
+      newUser.lastName = lastName.trim();
+      newUser.email = email.trim();
+      newUser.password = password.trim();
+      newUser.type = 'staff';
+      newUser.createdAt = new Date();
 
-        users.push(newUser);
-        const payload = { id: newUser.id, email: newUser.email };
-        const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1hr' });
-        const data = {
-          token,
-          id: newUser.id,
-          firstName: newUser.firstName,
-          lastName: newUser.lastName,
-          email: newUser.email,
-          type: newUser.type,
-          createdAt: newUser.createdAt
-        };
-        return res.status(201).json({
-          status: 201,
-          data,
-          message: 'User registered successfully'
-        });
-      }
-      res.status(409).json({
-        status: 409,
-        error: 'User already exists'
-      });
-    } catch (err) {
-      return res.status(500).json({
-        status: 500,
-        error: err.message
+      users.push(newUser);
+      const payload = { id: newUser.id, email: newUser.email };
+      const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1hr' });
+      const data = {
+        token,
+        id: newUser.id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+        type: newUser.type,
+        createdAt: newUser.createdAt
+      };
+      return res.status(201).json({
+        status: 201,
+        data,
+        message: 'User registered successfully'
       });
     }
+    res.status(409).json({
+      status: 409,
+      error: 'User already exists'
+    });
 
     return true;
   },
@@ -75,49 +68,41 @@ const AuthController = {
    * @access public
    */
   signIn(req, res) {
-    try {
-      const { email, password } = req.body;
-      for (let i = 0; i < users.length; i += 1) {
-        if (email === users[i].email) {
-          if (password === users[i].password) {
-            const userInfo = users[i];
-            const payload = {
-              id: userInfo.id,
-              email: userInfo.email,
-              type: userInfo.type
-            };
-            const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1hr' });
-            const data = {
-              token,
-              id: userInfo.id,
-              firstName: userInfo.firstName,
-              lastName: userInfo.lastName,
-              email: userInfo.email,
-              type: userInfo.type
-            };
-            return res.status(200).json({
-              status: 200,
-              data,
-              message: 'Login successful'
-            });
-          }
-          return res.status(403).json({
-            status: 403,
-            error: 'Password Incorrect'
+    const { email, password } = req.body;
+    for (let i = 0; i < users.length; i += 1) {
+      if (email === users[i].email) {
+        if (password === users[i].password) {
+          const userInfo = users[i];
+          const payload = {
+            id: userInfo.id,
+            email: userInfo.email,
+            type: userInfo.type
+          };
+          const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1hr' });
+          const data = {
+            token,
+            id: userInfo.id,
+            firstName: userInfo.firstName,
+            lastName: userInfo.lastName,
+            email: userInfo.email,
+            type: userInfo.type
+          };
+          return res.status(200).json({
+            status: 200,
+            data,
+            message: 'Login successful'
           });
         }
         return res.status(403).json({
           status: 403,
-          error: 'User not found'
+          error: 'Password Incorrect'
         });
       }
-    } catch (err) {
-      return res.status(500).json({
-        status: 500,
-        error: err.message
-      });
     }
-    return true;
+    return res.status(403).json({
+      status: 403,
+      error: 'User not found'
+    });
   }
 };
 
