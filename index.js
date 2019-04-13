90320f6d33ac9c5a0ce14b6dde6573eccdfed0ce
Tolulope Odueke <tolulope.od@gmail.com>
2019-04-13 12:43:16 +0100

    chore(endpoint):create error handling middleware
    Create middleware to handle server errors and respond with a descriptive message
    
    [Finishes #165293063]

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
+import logger from 'morgan';
+import Debug from 'debug';
 
 import swaggerDoc from './swagger.json';
 
@@ -10,6 +12,7 @@ import accountRoute from './server/routes/AccountRoute';
 import transactionRoute from './server/routes/TransactionRoute';
 
 const { log } = console;
+const debug = Debug('dev');
 
 // Set up the app with express
 const app = express();
@@ -22,6 +25,9 @@ const API_PREFIX = '/api/v1';
  */
 app.get('/', (req, res) => res.status(200).json({ message: 'BANKA APP API RESPONSE SUCCESSFUL' }));
 
+// logging middleware
+app.use(logger('dev'));
+
 // Body parser middleware
 app.use(bodyParser({ extended: false }));
 app.use(bodyParser.json());
@@ -33,6 +39,13 @@ app.use(`${API_PREFIX}/transactions`, transactionRoute);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.listen(PORT, () => log(`Server Running on Port ${PORT}`));
+// Handle errors
+app.use((err, req, res, next) => {
+  log(err.stack);
+  res.status(500).json({ error: err.message });
+  next();
+});
+
+app.listen(PORT, () => debug(`Server Running on Port ${PORT}`));
 
 export default app;
